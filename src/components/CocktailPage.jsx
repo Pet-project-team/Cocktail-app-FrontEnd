acba5f6ddcab42352d31af950437e8e345ab0aa5
oleksiiyovenko <aleksey.xmen@gmail.com>
2024-02-02 17:01:16 +0200

    feat: recipe and video functional

--- src/components/CocktailPage.jsx
+++ src/components/CocktailPage.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router";
 import "../styles/coctail-page.css";
 import coctailsList from "../cocktails.json";
 import ingredientsList from "../ingredients.json";
+import equipmentsList from "../equipments.json";
 
 export default function CocktailPage() {
   const params = useParams();
@@ -38,12 +39,12 @@ export default function CocktailPage() {
           <p>{cocktailPageInfo.cocktailDescription}</p>
           <div id="share-links">
             <p>Share</p>
-            <a href="">
+            <button type="button">
               <img src="/twitter.svg" alt="" />
-            </a>
-            <a href="">
+            </button>
+            <button type="button">
               <img src="/facebook.svg" alt="" />
-            </a>
+            </button>
             <button type="button">
               <img src="/content_copy.svg" alt="" />
             </button>
@@ -56,7 +57,7 @@ export default function CocktailPage() {
                 className={
                   "nav-info-button" +
                   " " +
-                  (tab == 1
+                  (tab === 1
                     ? "highlighted-cocktail-page-btn"
                     : "unhighlighted-cocktail-page-btn")
                 }
@@ -71,7 +72,7 @@ export default function CocktailPage() {
                 className={
                   "nav-info-button" +
                   " " +
-                  (tab == 2
+                  (tab === 2
                     ? "highlighted-cocktail-page-btn"
                     : "unhighlighted-cocktail-page-btn")
                 }
@@ -86,7 +87,7 @@ export default function CocktailPage() {
                 className={
                   "nav-info-button" +
                   " " +
-                  (tab == 3
+                  (tab === 3
                     ? "highlighted-cocktail-page-btn"
                     : "unhighlighted-cocktail-page-btn")
                 }
@@ -104,7 +105,7 @@ export default function CocktailPage() {
                 className={
                   "nav-info-button" +
                   " " +
-                  (measurement == 2
+                  (measurement === 2
                     ? "highlighted-cocktail-page-btn"
                     : "unhighlighted-cocktail-page-btn")
                 }
@@ -119,7 +120,7 @@ export default function CocktailPage() {
                 className={
                   "nav-info-button" +
                   " " +
-                  (measurement == 1
+                  (measurement === 1
                     ? "highlighted-cocktail-page-btn"
                     : "unhighlighted-cocktail-page-btn")
                 }
@@ -132,85 +133,133 @@ export default function CocktailPage() {
               </button>
             </div>
           </div>
-          {tab == 1 && (
-            <div id="cocktail-secondary-info__content">
-              <h1>Ingredients</h1>
-              <table>
-                <thead>
-                  <tr>
-                    <th scope="col"></th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Measurement</th>
-                    <th scope="col">Links</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {cocktailPageInfo.ingredients.map((ingredient) => (
+          {tab === 1 && (
+            <div id="ingredients-tab">
+              <div id="cocktail-secondary-info__content">
+                <h1>Ingredients</h1>
+                <table>
+                  <thead>
                     <tr>
-                      <th scope="col">
-                        <img
-                          src={ingredientsList[ingredient.ingredienId].photo}
-                        />
-                      </th>
-                      <th scope="col">
-                        <p>{ingredientsList[ingredient.ingredienId].name}</p>
-                      </th>
-                      <th scope="col">
-                        {ingredient.numberOZ && (
-                          <p>
-                            {measurement == 1
-                              ? ingredient.numberOZ + " oz "
-                              : ingredient.numberOZ * 30 + " ml "}
-                          </p>
-                        )}
-                        {ingredient.number && <p>{ingredient.number}</p>}
-                        {!(ingredient.numberOZ || ingredient.number) && (
-                          <p>-</p>
-                        )}
-                      </th>
-                      <th scope="col">
-                        <button type="button">Add to filter</button>
-                        {ingredient.haveLink && (
-                          <a
-                            href={ingredientsList[ingredient.ingredienId].url}
-                            target="_blank"
-                          >
-                            Buy
-                          </a>
-                        )}
-                      </th>
+                      <th scope="col"></th>
+                      <th scope="col">Name</th>
+                      <th scope="col">Measurement</th>
+                      <th scope="col">Links</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-              {/* <ul id="ingredients">
-              {cocktailPageInfo.ingredients.map((ingredient) => (
-                <li className="ingredient">
-                  {ingredient.haveLink && (
-                    <a
-                      href={ingredientsList[ingredient.ingredienId].url}
-                      target="_blank"
-                    >
-                      <img src="/openInNewTab.svg" alt="" />
-                    </a>
-                  )}
-                  <img src={ingredientsList[ingredient.ingredienId].photo} />
-                  {ingredient.numberOZ && (
-                    <p>
-                      {ingredient.numberOZ + " oz " + ingredient.ingredienTitle}
+                  </thead>
+                  <tbody>
+                    {cocktailPageInfo.ingredients.map((ingredient) => (
+                      <tr>
+                        <th scope="col">
+                          <img
+                            src={ingredientsList[ingredient.ingredienId].photo}
+                            alt={ingredientsList[ingredient.ingredienId].name}
+                          />
+                        </th>
+                        <th scope="col">
+                          <p>{ingredientsList[ingredient.ingredienId].name}</p>
+                        </th>
+                        <th scope="col">
+                          {ingredient.numberOZ && (
+                            <p>
+                              {measurement === 1
+                                ? ingredient.numberOZ + " oz "
+                                : ingredient.numberOZ * 30 + " ml "}
+                            </p>
+                          )}
+                          {ingredient.number && <p>{ingredient.number}</p>}
+                          {!(ingredient.numberOZ || ingredient.number) && (
+                            <p>-</p>
+                          )}
+                        </th>
+                        <th scope="col">
+                          <button type="button">Add to filter</button>
+                          {ingredient.haveLink && (
+                            <a
+                              href={ingredientsList[ingredient.ingredienId].url}
+                              target="_blank"
+                              rel="noreferrer"
+                            >
+                              Buy
+                            </a>
+                          )}
+                        </th>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                <h1>Equipment</h1>
+                <div id="equipments">
+                  <table>
+                    <thead>
+                      <tr>
+                        <th scope="col"></th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Links</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {cocktailPageInfo.equipments.map((equipment) => (
+                        <tr>
+                          <th scope="col">
+                            <img
+                              src={equipmentsList[equipment.equipmentId].photo}
+                              alt={equipmentsList[equipment.equipmentId].name}
+                            />
+                          </th>
+                          <th scope="col">
+                            <p>{equipmentsList[equipment.equipmentId].name}</p>
+                          </th>
+                          <th scope="col">
+                            <button type="button">Add to filter</button>
+                            {equipment.haveLink && (
+                              <a
+                                href={equipmentsList[equipment.equipmentId].url}
+                                target="_blank"
+                                rel="noreferrer"
+                              >
+                                Buy
+                              </a>
+                            )}
+                          </th>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          )}
+          {tab === 2 && (
+            <div id="recipe-tab">
+              {cocktailPageInfo.recipe.map((element) => (
+                <>
+                  {element.type === "p" && (
+                    <p id="recipe-step">
+                      {element.step && <span>{element.step}</span>}
+                      {" " + element.content}
                     </p>
                   )}
-                  {ingredient.number && (
-                    <p>{ingredient.number + " " + ingredient.ingredienTitle}</p>
+                  {element.type === "img" && (
+                    <img
+                      id="recipe-step-img"
+                      alt="related to step text"
+                      src={element.url}
+                    ></img>
                   )}
-                  {!(ingredient.numberOZ || ingredient.number) && (
-                    <p>{ingredient.ingredienTitle}</p>
-                  )}
-                </li>
+                </>
               ))}
-            </ul> */}
-              <h1>Equipment</h1>
-              <div id="equipments"></div>
+            </div>
+          )}
+          {tab === 3 && (
+            <div id="video-tab">
+              <iframe
+                width="100%"
+                src={cocktailPageInfo.youtubeURL}
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen="true"
+              ></iframe>
             </div>
           )}
         </div>
